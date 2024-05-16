50a38e15b1d14df447898a45b1c9e9bd9620437d
NatalieShif <natalia.shif@gmail.com>
2024-05-16 16:31:44 +0300

    nodejs farm

--- nodejs/file03Async.js
+++ nodejs/file03Async.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 let data1;
 
-fs.readFile('./data/avocado.txt','utf-8',(err,data1) => {
+fs.readFile('./data/avocado.txt','utf-8',(err,data) => {
 
     // err could be FALSY - when there is no ERROR
     // err will be some OBJECT (TRUTHY value) - when there is an error
@@ -12,7 +12,7 @@ fs.readFile('./data/avocado.txt','utf-8',(err,data1) => {
 
     } else {
 
-
+        data1 = data;
         console.log(`data1 inside the CALLBACK of readFile() = ${data1}`)
 
     }
@@ -21,3 +21,9 @@ fs.readFile('./data/avocado.txt','utf-8',(err,data1) => {
 })
 
 console.log(`data1 after readFile() = ${data1}`)
+
+setTimeout(() => {
+    
+    console.log(`\n\ndata1 after readFile() inside setTimeout = ${data1}`)
+
+},50)
